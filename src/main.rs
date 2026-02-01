03ae2d7f8017c301bbb4103e4332c4ad0b52b8ac
Adam Domanski <adomanski@gmail.com>
2026-02-01 22:21:08 +0100

    working sample

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use libpulse_binding::stream::Direction;
 use thiserror::Error;
 use reqwest::header::CONTENT_TYPE;
-use futures_util::StreamExt;
+use reqwest::blocking::Response;
+use symphonia::core::audio::SampleBuffer;
 use symphonia::core::codecs::{CODEC_TYPE_NULL, DecoderOptions};
 use symphonia::core::formats::FormatOptions;
-use symphonia::core::io::{MediaSourceStream, MediaSourceStreamOptions};
+use symphonia::core::io::{MediaSourceStream, MediaSourceStreamOptions, ReadOnlySource};
 use symphonia::core::meta::MetadataOptions;
 use symphonia::core::probe::Hint;
+use libpulse_simple_binding as pulse;
 
 #[derive(Error, Debug)]
 pub enum R357Error {
@@ -13,86 +16,96 @@ pub enum R357Error {
     NotMp3Stream,
     #[error("Connect error")]
     ConnectError(#[from] reqwest::Error),
+    #[error("PulseAudio error")]
+    PulseError(#[from] libpulse_binding::error::PAErr),
+    #[error("Symphonia error")]
+    DecodeError(#[from] symphonia::core::errors::Error),
 }
 
-#[tokio::main]
-async fn main() -> Result<(), R357Error> {
+// #[tokio::main]
+fn main() -> Result<(), R357Error> {
     println!("r357");
 
-    let response = reqwest::get("https://stream.radio357.pl/").await?;
+    let response = reqwest::blocking::get("https://stream.radio357.pl/")?;
     let status = response.status();
     println!("Status: {}", status);
     let content_type = response.headers().get(CONTENT_TYPE);
     println!("Header: {:?}", content_type);
-    match content_type {
-        Some(header_value) if header_value == "audio/mpeg" => {
-            let mut stream = response.bytes_stream();
-
-            let mut buf = Vec::with_capacity(8);
-
-            while let Some(chunk) = stream.next().await {
-                let chunk = chunk?;
-                let needed = 8 - buf.len();
-                buf.extend_from_slice(&chunk[..chunk.len().min(needed)]);
-                if buf.len() == 8 {
-                    break;
-                }
-            }
-
-            println!("{:X?}", buf);
-
-
-
-            Ok(())
-        }
-        _ => {
-            eprintln!("Content-Type not supported {}", content_type.unwrap().to_str().unwrap());
-            Err(R357Error::NotMp3Stream)
-        },
+    if content_type.is_none_or(|hv| hv != "audio/mpeg") {
+        eprintln!("Content-Type not supported {}", content_type.unwrap().to_str().unwrap());
+        return Err(R357Error::NotMp3Stream)
     }
-    // let bytes = request.bytes().await?;
-    // let bytes = bytes;
 
-    // println!("Bytes {:x?}", bytes);
+    let _ = play(response);
 
+    Ok(())
 }
 
-async fn play() {
+fn play(response: Response) -> Result<(), R357Error> {
+    let source = ReadOnlySource::new(response);
+    let mss = MediaSourceStream::new(
+        Box::new(source),
+        MediaSourceStreamOptions::default(),
+    );
+
     let mut hint = Hint::new();
     hint.mime_type("audio/mpeg");
-
-    let media_source = todo!();
-    let mss: MediaSourceStream = todo!();
 
     let meta_opts = MetadataOptions::default();
     let format_opts = FormatOptions::default();
 
     let probe = symphonia::default::get_probe()
-        .format(&hint, mss, &format_opts, &meta_opts)
-        .expect("unsupported format");
+        .format(&hint, mss, &format_opts, &meta_opts)?;
 
     let mut format = probe.format;
 
     let track = format.tracks()
         .iter()
-        .find(|t| t.codec_params.codec != CODEC_TYPE_NULL)
-        .expect("no supported audio tracks");
+        .find(|t| t.codec_params.codec != CODEC_TYPE_NULL).unwrap();
 
     // Use the default options for the decoder.
     let dec_opts: DecoderOptions = Default::default();
     let mut decoder = symphonia::default::get_codecs()
-        .make(&track.codec_params, &dec_opts)
-        .expect("decoder is needed");
+        .make(&track.codec_params, &dec_opts)?;
+
+    let mut sample_buf: Option<SampleBuffer<i16>> = None;
+
+    let spec = libpulse_binding::sample::Spec {
+        format: libpulse_binding::sample::Format::S16le,
+        channels: 2,
+        rate: 44100,
+    };
+
+    let p = pulse::Simple::new(
+        None,
+        "R357",
+        Direction::Playback,
+        None,
+        "Radio 357 Stream",
+        &spec,
+        None,
+        None,
+    )?;
 
     loop {
-        let packet = match format.next_packet() {
-            Ok(packet) => packet,
-            _ => panic!("panic"),
-        };
+        let packet = format.next_packet()?;
+        let decoded = decoder.decode(&packet)?;
 
-        match decoder.decode(&packet) {
-            Ok(decoded) => todo!(),
-            _ => panic!("panic"),
+        if sample_buf.is_none() {
+            sample_buf = Some(
+                SampleBuffer::<i16>::new(
+                    decoded.capacity() as u64,
+                    *decoded.spec(),
+                )
+            )
         }
+
+        let buf = sample_buf.as_mut().unwrap();
+        buf.copy_interleaved_ref(decoded);
+
+        let pcm: &[i16] = buf.samples();
+        let pcm_u8: &[u8] = bytemuck::cast_slice(pcm);
+
+        p.write(pcm_u8)?;
     }
 }
